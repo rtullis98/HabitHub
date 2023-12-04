65c70818dc52c7dce2b0400b9a4c7e5b17efcf77
rtullis98 <121829715+rtullis98@users.noreply.github.com>
2023-12-04 14:12:05 -0600

    navbar setup

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,4 +1,3 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import Link from 'next/link';
 import {
@@ -14,19 +13,28 @@ export default function NavBar() {
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
         <Link passHref href="/">
-          <Navbar.Brand>CHANGE ME</Navbar.Brand>
+          <Navbar.Brand className="fs-3">Habit Hub</Navbar.Brand>
         </Link>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
+          <Nav className="ps-5">
             {/* CLOSE NAVBAR ON LINK SELECTION: https://stackoverflow.com/questions/72813635/collapse-on-select-react-bootstrap-navbar-with-nextjs-not-working */}
             <Link passHref href="/">
               <Nav.Link>Home</Nav.Link>
             </Link>
-            <Link passHref href="/delete-me">
-              <Nav.Link>Delete Me</Nav.Link>
+            <Link passHref href="/allPostsPage">
+              <Nav.Link>All Posts</Nav.Link>
             </Link>
-            <Button variant="danger" onClick={signOut}>
+            <Link passHref href="/createPostPage">
+              <Nav.Link>Add a Post</Nav.Link>
+            </Link>
+            <Link passHref href="/myPostsPage">
+              <Nav.Link>My Posts</Nav.Link>
+            </Link>
+            <Link passHref href="/userProfile">
+              <Nav.Link>User Profile</Nav.Link>
+            </Link>
+            <Button className="me-5" variant="dark fw-bold ps-3" onClick={signOut}>
               Sign Out
             </Button>
           </Nav>
